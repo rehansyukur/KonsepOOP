a864fac1abd94fdac15b017361131e9527bf00d8
rehansyukur <smkiu.009@gmail.com>
2025-07-24 14:18:50 +0700

    Tamabah Product

--- Konsep_OOP/web.php
+++ Konsep_OOP/web.php
@@ -15,3 +15,4 @@ class Product
 
 $product1 = new Product('Smartphone', 5000000, 'Smartphone terbaru dengan kamera berkualitas tinggi');
 $product2 = new Product('Realme', 4000000, 'Smartphone terbaru dengan build quality terbaik');
+$product2 = new Product('Realme', 4000000, 'Smartphone terbaru dengan build quality terbaik');
